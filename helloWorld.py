63f4371e1d7cc75ec98a0e0f4a9cf11380cdb0af
rituraj <riturajroj1234@gmail.com>
2022-04-01 19:28:44 +0530

    done

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+print('Hello\n', 'India!!') # "\n" for new line 
+
+#varuables in python
+name = 'ratu'
+age = 19
+print(name)
+
+#concatenation
+print(name+' is',age, 'year old halfengineer from badlapur!')
