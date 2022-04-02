a6efcd25e66b6b92e64a58c7ebc99cdfb2d84d65
rituraj <riturajroj1234@gmail.com>
2022-04-02 16:39:07 +0530

    return

---
+++ return.py
@@ -0,0 +1,6 @@
+def rituraj(num1, num2):
+
+    return num1 + num2
+num1 = float(input("Enter number 1 : ")) #define the data types(int, float, str,,....), else it will print it as string 
+num2 = float(input("Enter number 2 : "))# try this code without defing data types
+print(rituraj(num1, num2))
