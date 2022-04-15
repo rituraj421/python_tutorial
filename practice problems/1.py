4ecb59b4bca08d08f3c015343dff73e6d1feda35
rituraj <riturajroj1234@gmail.com>
2022-04-15 22:09:02 +0530

    main

---
+++ practice problems/1.py
@@ -0,0 +1,19 @@
+#Write a program to demonstrate different number data types in python.
+
+x = 20     #int
+
+print(x)
+
+print(type(x))
+
+x = 20.5 #float
+
+print(x)
+
+print(type(x))
+
+x = 1j        #complex
+
+print(x)
+
+print(type(x))
